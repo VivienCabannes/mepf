3c4936c43f575566ff40021ca1ddf68a3fce0a5c
Vivien Cabannes <vivien.cabannes@gmail.com>
2023-12-05 10:23:38 -0500

    stats for empirical mode

--- src/entsearch/search/exhaustive.py
+++ src/entsearch/search/exhaustive.py
@@ -1,31 +1,53 @@
-
 import numpy as np
-from ..huffman import huffman_codes
 
 
-def dichotomic_baseline(Y):
+def get_nb_queries(y_cat, codes):
     """
     Perform dichotomic search on the given sequence.
 
     Parameters
     ----------
-    Y : numpy.ndarray
+    y_cat : numpy.ndarray of int
         Sequence of observations.
+    codes: numpy.ndarray of int
+        Codes of each symbol.
 
     Returns
     -------
-    T : numpy.ndarray
-        Array of the number of queries.
-    mode : numpy.ndarray
-        Array of the estimated modes.
-    p_mode : numpy.ndarray
-        Array of the estimated probabilities of the modes.
+    nb_queries : numpy.ndarray
+        Number of queries make to identify each y.
     """
-    n, m = Y.shape
-    S = huffman_codes([1 for i in range(m)])
-    T = (Y @ S != -1).sum(axis=1).cumsum()
-    p_hat = Y.cumsum(axis=0)
+    n = len(y_cat)
+    m = len(codes)
+    y_one_hot = np.zeros((n, m))
+    y_one_hot[np.arange(n), y_cat] = 1
+    nb_queries = (y_one_hot @ codes != -1).sum(axis=1)
+    return nb_queries
+
+
+def get_exhaustive_statistics(y_cat, m):
+    """
+    Compute sequences of empirical modes and their empirical frequencies.
+
+    Parameters
+    ----------
+    y_cat : numpy.ndarray of int
+        Sequence of observations.
+    m : int
+        Number of symbols.
+
+    Returns
+    -------
+    hat_mode : numpy.ndarray of int
+        Sequence of empirical modes.
+    hat_p_mode : numpy.ndarray of float
+        Sequence of empirical frequencies.
+    """
+    n = len(y_cat)
+    p_hat = np.zeros((n, m))
+    p_hat[np.arange(n), y_cat] = 1
+    np.cumsum(p_hat, out=p_hat, axis=0)
     p_hat /= np.arange(1, n + 1)[:, np.newaxis]
-    mode = np.argmax(p_hat, axis=1)
-    p_mode = p_hat[np.arange(n), mode]
-    return T, mode, p_mode
+    hat_mode = np.argmax(p_hat, axis=1)
+    hat_p_mode = p_hat[np.arange(n), hat_mode]
+    return hat_mode, hat_p_mode
